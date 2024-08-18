15086e66354f3b050c68befa6544a6f22df978ab
faizkhan12 <faizk6797@gmail.com>
2024-08-18 19:45:01 +0530

    enable https

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -9,7 +9,7 @@ interface ApiError {
 export const api = axios.create({
   baseURL:
     import.meta.env.MODE !== "development"
-      ? "http://graphrag-backend.bluedot.click/api"
+      ? "https://graphrag-backend.bluedot.click/api"
       : `http://localhost:5173/api`,
 });
 
