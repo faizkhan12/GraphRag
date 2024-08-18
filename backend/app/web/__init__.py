09736757be7ec030382d0bc961805b69e3fc0761
faizkhan12 <faizk6797@gmail.com>
2024-08-18 16:51:45 +0530

    add 404 route

--- backend/app/web/__init__.py
+++ backend/app/web/__init__.py
@@ -14,13 +14,17 @@ from app.web.views import (
 
 
 def create_app():
-    app = Flask(__name__, static_folder="../../../frontend/build")
+    app = Flask(__name__)
     app.url_map.strict_slashes = False
     app.config.from_object(Config)
-    app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024   
     register_extensions(app)
     register_hooks(app)
     register_blueprints(app)
+    
+    @app.errorhandler(404)
+    def page_not_found(e):
+        # Return a custom 404 JSON response or render a template
+        return {"error": "This page does not exist. Please check the URL."}, 404
 
     return app
 
@@ -42,3 +46,4 @@ def register_hooks(app):
     app.before_request(load_logged_in_user)
     app.after_request(add_headers)
     app.register_error_handler(Exception, handle_error)
+
