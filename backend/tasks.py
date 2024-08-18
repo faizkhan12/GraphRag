f5e486313cbf5172d7045ab1f1498cdaea87ae05
faizkhan12 <faizk6797@gmail.com>
2024-08-18 17:56:36 +0530

    revert host name

--- backend/tasks.py
+++ backend/tasks.py
@@ -5,7 +5,7 @@ from invoke import task
 @task
 def dev(ctx):
     ctx.run(
-        "flask --app app.web run --port 8000 --host 0.0.0.0",
+        "flask --app app.web run --port 8000 ",
         pty=os.name != "nt",
         env={"APP_ENV": "development"},
     )
